112c25147ea5a4826ccecf1dd144e76604614434
Rating <achubenko@rating.kz>
2023-11-29 14:42:27 +0600

    Initial Commit

---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Rating on 29.11.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Rating on 29.11.2023.
+//
+
+import Foundation
